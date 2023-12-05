1c6c1daa956fa09c141e835e49a28fb6c3254be8
ishanka158 <75593569+ishanka158@users.noreply.github.com>
2023-12-05 21:05:06 +0530

    Update firebase.js

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -10,13 +10,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDnR1ftFVLNYimQdiTtlxKytyLsTzl2OvQ",
-  authDomain: "insta-clone-dce1a.firebaseapp.com",
-  projectId: "insta-clone-dce1a",
-  storageBucket: "insta-clone-dce1a.appspot.com",
-  messagingSenderId: "490833275526",
-  appId: "1:490833275526:web:09fb2e5a783b2ccd3eff07",
-  measurementId: "G-6J1QXR4KCX",
+  
 };
 
 // Initialize Firebase
