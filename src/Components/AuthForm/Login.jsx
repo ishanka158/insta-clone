8c84910a4b8f2500d7e907e5787983e5bc7e8b6b
ishanka158 <96714@fhss.sjp.ac.lk>
2023-12-07 17:19:26 +0530

    implemented google authantication

--- src/Components/AuthForm/Login.jsx
+++ src/Components/AuthForm/Login.jsx
@@ -7,25 +7,23 @@ const Login = () => {
     email: "",
     password: "",
   });
-
-  const [loading, error, login] = useLogin();
-
+  const { loading, error, login } = useLogin();
   return (
     <>
       <Input
         placeholder="Email"
         fontSize={14}
         type="email"
-        value={inputs.email}
         size={"sm"}
+        value={inputs.email}
         onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
       />
       <Input
         placeholder="Password"
         fontSize={14}
+        size={"sm"}
         type="password"
         value={inputs.password}
-        size={"sm"}
         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
       />
       {error && (
@@ -42,7 +40,7 @@ const Login = () => {
         isLoading={loading}
         onClick={() => login(inputs)}
       >
-        Log In
+        Log in
       </Button>
     </>
   );
